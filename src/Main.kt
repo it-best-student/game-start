30ff0c2b3ca93fa3a73fe1e898f58e21653df683
esmendero <elurgencia96@gmail.com>
2019-11-21 00:01:38 +0200

    Конец последнего урока

--- src/Main.kt
+++ src/Main.kt
@@ -1,10 +1,3 @@
 fun main(args:Array<String>){
-  var fight = Fight()
-    fight.fight(fight.skeleton)
-    fight.fight(fight.skeleton)
-    fight.fight(fight.skeleton)
-    fight.fight(fight.skeleton)
-    fight.fight(fight.skeleton)
-    fight.fight(fight.skeleton)
 
 }
