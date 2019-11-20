2fcc8ee2026d3d3c37290f0646d3836d12f8e106
esmendero <elurgencia96@gmail.com>
2019-11-20 23:59:18 +0200

    Тест перехода по коммитам

--- src/Main.kt
+++ src/Main.kt
@@ -2,4 +2,9 @@ fun main(args:Array<String>){
   var fight = Fight()
     fight.fight(fight.skeleton)
     fight.fight(fight.skeleton)
+    fight.fight(fight.skeleton)
+    fight.fight(fight.skeleton)
+    fight.fight(fight.skeleton)
+    fight.fight(fight.skeleton)
+
 }
